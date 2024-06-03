9553e2ab4649376e7c2863d897be4e0027e3a26a
Phil Patterson <philip.james.patterson@gmail.com>
2024-06-03 20:48:19 +1000

    hey - its working... kinda.  DuckDB is loading, next postgres IO manager

--- dagster_finance/assets.py
+++ dagster_finance/assets.py
@@ -36,14 +36,13 @@ from .constants import dbt_manifest_path
 #     def get_asset_key(self, dbt_resource_props: Mapping[str, Any]) -> AssetKey:
 #         return super().get_asset_key(dbt_resource_props).with_prefix("qif_files")
 
-dagster_dbt_translator = DagsterDbtTranslator(
-    settings=DagsterDbtTranslatorSettings(enable_asset_checks=False)
-)
+# dagster_dbt_translator = DagsterDbtTranslator(
+#     settings=DagsterDbtTranslatorSettings(enable_asset_checks=False)
+# )
 
 
 @dbt_assets(
     manifest=dbt_manifest_path,
-    dagster_dbt_translator=dagster_dbt_translator,
 )
 def finance_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
@@ -73,81 +72,102 @@ def create_composite_key(row: Dict[str, str]) -> Optional[str]:
 # TODO the input should be each file in the QIF and the output should be an individual DF.
 
 
-@asset(compute_kind="python", io_manager_key="io_manager")
+@asset(compute_kind="python")
 def upload_dataframe_to_duck_db(df: pd.DataFrame) -> pd.DataFrame:
     """Setup to capture individual df within the io manager"""
     return df
 
+def convert_qif_to_df(qif_file: Path) -> pd.DataFrame:
+    qif_processor = quiffen.Qif.parse(qif_file, day_first=False)
+    df = qif_processor.to_dataframe()
 
-@multi_asset(compute_kind="python",
-             outs={
-                 "Adelaide_Homeloan_Transactions": AssetOut(),
-                 "Adelaide_Offset_Transactions": AssetOut(),
-                 "Bendigo_Bank_Homeloan_Transactions": AssetOut(),
-                 "Bendigo_Bank_Offset_Transactions": AssetOut(),
-                 "ING_BillsBillsBills_Transactions": AssetOut(),
-                 "ING_Countdown_Transactions": AssetOut()
-             })
-def QIF_to_DF(context: AssetExecutionContext) -> Dict[str, pd.DataFrame]:
-    """
-    Converts Quicken Interchange Format (QIF) files in a folder to Pandas DataFrames.
-
-    Parameters:
-    - folder_path (str): The path to the folder containing QIF files.
-
-    The account name (used for the SQL table name) will be the filename.
-
-    Returns:
-    - Dict[str, Optional[pd.DataFrame]]: A dictionary where keys are account names and values are DataFrames.
-    """
-
-    qif_folder_path = Path("qif_files")
-
-    dictionary_of_dfs = {}
-    list_of_source_qif_files = list(qif_folder_path.glob("*.qif"))
-
-    for qif_file in list_of_source_qif_files:
-        logger.debug(f"Processing qif file: {qif_file}")
-
-        account_name = Path(qif_file).stem
-
-        qif_processor = quiffen.Qif.parse(qif_file, day_first=False)
-
-        df = qif_processor.to_dataframe()
+    if df is not None:
         df.dropna(how="all", axis=1, inplace=True)
 
         # add a primary key
-        df["composite_key"] = df.apply(lambda row: create_composite_key(row), axis=1)  # type: ignore
+        df["composite_key"] = df.apply(lambda row: create_composite_key(row), axis=1)
 
         # add an ingestion timestamp
         df["ingestion_date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        raise ValueError(f"Datafrmae is empty for {qif_file}")
 
-        upload_dataframe_to_duck_db(df)
-
-        # collate df into a dict of dfs.
-        dictionary_of_dfs[account_name] = df
-
-        context.add_output_metadata(
-            metadata={
-                "num_records": len(df),  # Metadata can be any key-value pair
-                "preview": MetadataValue.md(df.head().to_markdown()),
-                # The `MetadataValue` class has useful static methods to build Metadata
-            }
-        )
-
-    return dictionary_of_dfs
+    return df
 
 
-@multi_asset(compute_kind="python",
-             outs={
-                 "Adelaide_Homeloan_Transactions": AssetOut(),
-                 "Adelaide_Offset_Transactions": AssetOut(),
-                 "Bendigo_Bank_Homeloan_Transactions": AssetOut(),
-                 "Bendigo_Bank_Offset_Transactions": AssetOut(),
-                 "ING_BillsBillsBills_Transactions": AssetOut(),
-                 "ING_Countdown_Transactions": AssetOut()
-             },
-             required_resource_keys={"pgConnection"})
+@asset(compute_kind="python")
+def upload_adelaide_homeloan(context: AssetExecutionContext):
+    df = convert_qif_to_df(Path('qif_files/Adelaide_Homeloan_Transactions.qif'))
+    context.add_output_metadata(
+    metadata={
+        "num_records": len(df),  # Metadata can be any key-value pair
+        "preview": MetadataValue.md(df.head().to_markdown()),
+        # The `MetadataValue` class has useful static methods to build Metadata
+        }
+    )   
+    return df
+    
+@asset(compute_kind="python")
+def upload_adelaide_offset(context: AssetExecutionContext):
+    df = convert_qif_to_df(Path('qif_files/Adelaide_Offset_Transactions.qif'))
+    context.add_output_metadata(
+    metadata={
+        "num_records": len(df),  # Metadata can be any key-value pair
+        "preview": MetadataValue.md(df.head().to_markdown()),
+        # The `MetadataValue` class has useful static methods to build Metadata
+        }
+    )   
+    return df
+
+@asset(compute_kind="python")
+def upload_bendigo_bank_homeloan(context: AssetExecutionContext):
+    df = convert_qif_to_df(Path('qif_files/Bendigo_Bank_Homeloan_Transactions.qif'))
+    context.add_output_metadata(
+    metadata={
+        "num_records": len(df),  # Metadata can be any key-value pair
+        "preview": MetadataValue.md(df.head().to_markdown()),
+        # The `MetadataValue` class has useful static methods to build Metadata
+        }
+    )   
+    return df
+
+@asset(compute_kind="python")
+def upload_bendigo_bank_offset(context: AssetExecutionContext):
+    df = convert_qif_to_df(Path('qif_files/Bendigo_Bank_Offset_Transactions.qif'))
+    context.add_output_metadata(
+    metadata={
+        "num_records": len(df),  # Metadata can be any key-value pair
+        "preview": MetadataValue.md(df.head().to_markdown()),
+        # The `MetadataValue` class has useful static methods to build Metadata
+        }
+    )   
+    return df
+
+@asset(compute_kind="python")
+def upload_ing_billsbillsbills(context: AssetExecutionContext):
+    df = convert_qif_to_df(Path('qif_files/ING_BillsBillsBills_Transactions.qif'))
+    context.add_output_metadata(
+    metadata={
+        "num_records": len(df),  # Metadata can be any key-value pair
+        "preview": MetadataValue.md(df.head().to_markdown()),
+        # The `MetadataValue` class has useful static methods to build Metadata
+        }
+    )   
+    return df
+
+@asset(compute_kind="python")
+def upload_ing_countdown(context: AssetExecutionContext):
+    df = convert_qif_to_df(Path('qif_files/ING_Countdown_Transactions.qif'))
+    context.add_output_metadata(
+    metadata={
+        "num_records": len(df),  # Metadata can be any key-value pair
+        "preview": MetadataValue.md(df.head().to_markdown()),
+        # The `MetadataValue` class has useful static methods to build Metadata
+        }
+    )   
+    return df
+
+
 def upload_dataframe_to_postgres(
     context, QIF_to_DF: Dict[str, Optional[pd.DataFrame]], my_conn: pgConnection
 ) -> None:
--- dagster_finance/definitions.py
+++ dagster_finance/definitions.py
@@ -7,23 +7,30 @@ from dagster_finance.resources import pgConnection
 from dagster_duckdb_pandas import duckdb_pandas_io_manager
 
 from .assets import (
-    QIF_to_DF,
     finance_dbt_assets,
-    ingest_dataframe_to_duckdb,
-    upload_dataframe_to_postgres,
+    upload_ing_countdown,
+    upload_ing_billsbillsbills,
+    upload_bendigo_bank_offset,
+    upload_bendigo_bank_homeloan,
+    upload_adelaide_offset,
+    upload_adelaide_homeloan,
 )
 from .constants import DBT_PROJECT_DIR
 
 
 defs = Definitions(
     assets=[
-        QIF_to_DF,
+        upload_ing_countdown,
+        upload_ing_billsbillsbills,
+        upload_bendigo_bank_offset,
+        upload_bendigo_bank_homeloan,
+        upload_adelaide_offset,
+        upload_adelaide_homeloan,
         finance_dbt_assets,
-        upload_dataframe_to_postgres,
     ],  # ingest_dataframe_to_duckdb,
     resources={
-        "postgres_db": pgConnection(EnvVar("POSTGRES_CONN_STR")),
+        #"postgres_db": pgConnection(EnvVar("POSTGRES_CONN_STR")),
         "dbt": DbtCliResource(project_dir=os.fspath(DBT_PROJECT_DIR)),
-        "io_manager": duckdb_pandas_io_manager(database="duckdb/finance.duckdb", schema="finance"),
+        "io_manager": duckdb_pandas_io_manager.configured({"database":"duckdb/finance.duckdb", "schema":"finance.raw"}),
         }
     )
