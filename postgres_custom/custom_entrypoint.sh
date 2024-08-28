5a5fd3615bf6539f998fd1aed07d6ca347a640ef
Philip Patterson <philip.james.patterson@gmail.com>
2024-08-28 04:47:20 +0000

    fixed postgres setup to add grafana user

--- postgres_custom/custom_entrypoint.sh
+++ postgres_custom/custom_entrypoint.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 set -e
 
-# Start the original entrypoint script with PostgreSQL in the background
-docker-entrypoint.sh postgres &
+# Call default entrypoint to handle the database initialization
+/usr/local/bin/docker-entrypoint.sh postgres &
 
-# Wait for PostgreSQL to start
-until pg_isready -U ${POSTGRES_USER} -d ${POSTGRES_DB}; do
-  echo "Waiting for PostgreSQL to start..."
-  sleep 1
+# Wait for PostgreSQL to start and be ready for connections
+echo "Waiting for PostgreSQL to become active..."
+until pg_isready -h localhost -p 5432 -U "${POSTGRES_USER}"; do
+    echo "localhost:5432 - no response"
+    sleep 1
 done
 
-# Run custom SQL commands
+echo "PostgreSQL is active."
+
+echo "Using GRAFANA_USER: ${GRAFANA_USER} with GRAFANA_PASSWORD: ${GRAFANA_PASSWORD}"
+
+# Check if user exists and create if not
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
-    CREATE USER grafanareader WITH PASSWORD 'test';
-    GRANT USAGE ON SCHEMA public TO grafanareader;
-    GRANT SELECT ON ALL TABLES IN SCHEMA public TO grafanareader;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO grafanareader;
+DO
+\$\$
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '${GRAFANA_USER}') THEN
+        ALTER USER ${GRAFANA_USER} WITH PASSWORD '${GRAFANA_PASSWORD}';
+        -- Optionally update privileges if they might have changed
+        GRANT USAGE ON SCHEMA reporting TO ${GRAFANA_USER};
+        GRANT SELECT ON ALL TABLES IN SCHEMA reporting TO ${GRAFANA_USER};
+        ALTER DEFAULT PRIVILEGES IN SCHEMA reporting GRANT SELECT ON TABLES TO ${GRAFANA_USER};
+    ELSE
+        CREATE USER ${GRAFANA_USER} WITH PASSWORD '${GRAFANA_PASSWORD}';
+        GRANT USAGE ON SCHEMA reporting TO ${GRAFANA_USER};
+        GRANT SELECT ON ALL TABLES IN SCHEMA reporting TO ${GRAFANA_USER};
+        ALTER DEFAULT PRIVILEGES IN SCHEMA reporting GRANT SELECT ON TABLES TO ${GRAFANA_USER};
+    END IF;
+END
+\$\$;
 EOSQL
 
-# Keep PostgreSQL running in the foreground
+echo "User and permissions setup complete."
+
+# Keep the process running
 wait
