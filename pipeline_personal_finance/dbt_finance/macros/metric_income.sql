13794b9d36a29e192e591e4351cc171e0de43e91
Philip Patterson <philip.james.patterson@gmail.com>
2026-02-02 02:01:04 +0000

    dbt: remove unused metric macros

--- pipeline_personal_finance/dbt_finance/macros/metric_income.sql
+++
@@ -1,8 +0,0 @@
-{% macro metric_income(table_alias='ft') %}
-  CASE
-    WHEN COALESCE({{ table_alias }}.is_internal_transfer, FALSE) THEN 0
-    WHEN COALESCE({{ table_alias }}.is_income_transaction, FALSE) THEN ABS({{ table_alias }}.transaction_amount)
-    WHEN {{ table_alias }}.transaction_amount > 0 THEN ABS({{ table_alias }}.transaction_amount)
-    ELSE 0
-  END
-{% endmacro %}
